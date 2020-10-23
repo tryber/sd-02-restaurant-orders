77af4f09aa2db8151c5fe3425c911e022200c3fc
Andrey <andreytsuzuki@gmail.com>
2020-10-23 14:12:36 -0300

    tests

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,15 +1,42 @@
+from src.analyse_log import (
+    fill_clients_with_row,
+    most_ordered,
+    get_order_qt,
+    never_ordered,
+    week_days_never_visited,
+)
+
+
 class TrackOrders:
+    def __init__(self):
+        self.clients = {}
+        self.all_orders = set()
+        self.all_week_days = set()
+
     def add_new_order(self, costumer, order, day):
-        pass
+        row = [costumer, order, day]
+        self.all_orders.add(order)
+        self.all_week_days.add(day)
+
+        fill_clients_with_row(
+            row,
+            self.clients,
+            self.all_orders,
+            self.all_week_days,
+        )
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        return most_ordered(costumer, self.clients)
 
     def get_order_frequency_per_costumer(self, costumer, order):
-        pass
+        return get_order_qt(order, costumer, self.clients)
 
     def get_never_ordered_per_costumer(self, costumer):
-        pass
+        return never_ordered(self.clients, costumer, self.all_orders)
 
     def get_days_never_visited_per_costumer(self, costumer):
-        pass
+        return week_days_never_visited(
+            self.clients,
+            costumer,
+            self.all_week_days,
+        )
--- tests/test_analyse_log.py
+++ tests/test_analyse_log.py
@@ -1,2 +1,11 @@
-# Crie uma suíte de testes para o método analyse_log
-# Obtenha, no mínimo, 90% de cobertura
+from src.analyse_log import analyse_log
+
+
+def test_analyse_log():
+    analyse_log('./test.txt')
+
+    with open('./test.txt', "r") as file:
+        assert 'hamburguer\n' == file.readline()
+        assert '0\n' == file.readline()
+        assert {'pizza', 'coxinha', 'misto-quente'} == eval(file.readline())
+        assert {'sabado', 'segunda-feira'} == eval(file.readline())
