81724a33c0fb2af077014bfa6ec90f8c11fa07a8
eddyeboy27 <lipe0503@gmail.com>
2020-10-26 02:26:29 -0300

    finalizado maria, arnaldo e joao orders

--- src/analyse_log.py
+++ src/analyse_log.py
@@ -3,7 +3,7 @@ import csv
 from os import path
 
 
-def define_orders(file):
+def open_orders(file):
     orders_list = []
 
     with open(file, "r") as csv_orders:
@@ -18,8 +18,45 @@ def define_orders(file):
 
 
 def maria_orders(file):
-    all_orders = define_orders(file)
-    print(all_orders)
+    all_orders = open_orders(file)
+    maria_orders = []
+    repeated_value = {}
+
+    for order in all_orders:
+        if order["client"] == "maria":
+            maria_orders.append(order)
+    for order in maria_orders:
+        if order["meal"] not in repeated_value:
+            repeated_value[order["meal"]] = 1
+        elif order["meal"] in repeated_value:
+            repeated_value[order["meal"]] += 1
+    more_requested = max(repeated_value, key=repeated_value.get)
+    return more_requested
+
+
+def arnaldo_orders(file):
+    all_orders = open_orders(file)
+    arnaldo_count = 0
+
+    for order in all_orders:
+        if order["client"] == "arnaldo" and order["meal"] == "hamburguer":
+            arnaldo_count += 1
+    return arnaldo_count
+
+
+def joao_orders(file):
+    all_orders = open_orders(file)
+    requests = set()
+    joao_delivered = set()
+
+    for order in all_orders:
+        if order["meal"] not in requests:
+            requests.add(order["meal"])
+    
+    for order in all_orders:
+        if order["client"] == "joao":
+            joao_delivered.add(order["meal"])
+    return requests.difference(joao_delivered)
 
 
 def analyse_log(path_to_file):
@@ -31,9 +68,11 @@ def analyse_log(path_to_file):
     if not path_to_file.endswith(".csv"):
         print("Formato Inválido", file=sys.stderr)
         return
-    
+
     organized_orders = [
-        maria_orders(path_to_file)
+        maria_orders(path_to_file),
+        arnaldo_orders(path_to_file),
+        joao_orders(path_to_file),
     ]
     return organized_orders
 
