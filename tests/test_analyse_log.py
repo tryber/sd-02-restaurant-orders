13dd75c122e6fa3508cbd0b36535aabce793dbe5
Bolivar <webdevelopmentbolivar@gmail.com>
2020-11-16 16:16:45 -0300

    test analyse log modules

--- tests/test_analyse_log.py
+++ tests/test_analyse_log.py
@@ -1,9 +1,44 @@
-from src.analyse_log import analyse_log
+from src.analyse_log import (analyse_log, get_maria_favorite_order, get_no_joao_days,
+                             get_no_joao_orders, how_many_times_arnaldo_ordered_hamburguer)
 from src.constants import (
     path_not_exists_csv, path_exists_csv, path_txt, expected_report)
 import pytest
 import os
 from unittest.mock import patch
+
+
+def test_analyse_log_file_not_exist():
+    with pytest.raises(ValueError,
+                       match="Arquivo not_exist.csv não encontrado"):
+        analyse_log(path_not_exists_csv)
+
+
+def test_get_no_joao_orders():
+    data = [{"name": "joao", "order": "amora", "day": "segunda-feira"},
+            {"name": "maria", "order": "peixe", "day": "quarta-feira"},
+            {"name": "joao", "order": "torta", "day": "quinta-feira"}]
+    assert get_no_joao_orders(data) == '- peixe'
+
+
+def test_get_no_joao_days():
+    data = [{"name": "joao", "order": "amora", "day": "segunda-feira"},
+            {"name": "maria", "order": "peixe", "day": "quarta-feira"},
+            {"name": "joao", "order": "torta", "day": "quinta-feira"}]
+    assert get_no_joao_days(data) == '- quarta-feira'
+
+
+def test_get_maria_favorite_order():
+    data = [{"name": "maria", "order": "peixe", "day": "segunda-feira"},
+            {"name": "maria", "order": "peixe", "day": "quarta-feira"},
+            {"name": "maria", "order": "torta", "day": "segunda-feira"}]
+    assert get_maria_favorite_order(data) == '- peixe'
+
+
+def test_how_many_times_arnaldo_ordered_hamburguer():
+    data = [{"name": "arnaldo", "order": "hamburguer", "day": "segunda-feira"},
+            {"name": "arnaldo", "order": "peixe", "day": "quarta-feira"},
+            {"name": "arnaldo", "order": "hamburguer", "day": "segunda-feira"}]
+    assert how_many_times_arnaldo_ordered_hamburguer(data) == '- 2'
 
 
 def test_analyse_log_file_not_exist():
